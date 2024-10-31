524c41fb7af894f86a04b7c064241470985328b0
Rauf <46323809+raufalili@users.noreply.github.com>
2024-10-30 22:47:00 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+
+var randomDiceImageSource1 = "images/" + randomDiceImage1;
+
+document.querySelector(".img1").setAttribute("src", randomDiceImageSource1);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomDiceImageSource2 = "images/" + randomDiceImage2;
+
+document.querySelector(".img2").setAttribute("src", randomDiceImageSource2);
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else if(randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
